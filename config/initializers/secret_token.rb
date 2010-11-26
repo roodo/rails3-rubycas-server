b33c27bed142e56d1570ea9a0485c71c52d763fa
Wuhsien.Yu <geniusyu@gmail.com>
2010-11-26 17:36:46 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoodoCasServer::Application.config.secret_token = 'd33c77683ec9ca4fc7e9cb3f5ca5ec122518a4547eee47f5c920baf6719f8793ab384dcf7abcf5123f6d66391ee1e16f740c338fd584ba9ec4232b9ba588c552'
