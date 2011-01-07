3821d53905b042512d5b055564aeb55f2d3bb29d
Wuhsien.Yu <geniusyu@gmail.com>
2011-01-07 15:44:40 +0800

    bug fix

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -369,25 +369,4 @@ class ServerController < ApplicationController
     end
   end
   
-  # def response_status_from_error(error)
-  #   case error.code.to_s
-  #   when /^INVALID_/, 'BAD_PGT'
-  #     422
-  #   when 'INTERNAL_ERROR'
-  #     500
-  #   else
-  #     500
-  #   end
-  # end
-  # 
-  # def serialize_extra_attribute(builder, value)
-  #   if value.kind_of?(String)
-  #     builder.text! value
-  #   elsif value.kind_of?(Numeric)
-  #     builder.text! value.to_s
-  #   else
-  #     builder.cdata! value.to_yaml
-  #   end
-  # end
-  
 end
--- app/helpers/server_helper.rb
+++ app/helpers/server_helper.rb
@@ -1,24 +1,4 @@
 # encoding: utf-8
 
 module ServerHelper
-  def response_status_from_error(error)
-    case error.code.to_s
-    when /^INVALID_/, 'BAD_PGT'
-      422
-    when 'INTERNAL_ERROR'
-      500
-    else
-      500
-    end
-  end
-
-  def serialize_extra_attribute(builder, value)
-    if value.kind_of?(String)
-      builder.text! value
-    elsif value.kind_of?(Numeric)
-      builder.text! value.to_s
-    else
-      builder.cdata! value.to_yaml
-    end
-  end
 end
