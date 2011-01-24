34ef0cbee6f3f0c276ee5eeeed03d68ee7433414
Wuhsien.Yu <geniusyu@gmail.com>
2011-01-24 10:21:10 +0800

    Add Google Authenticators

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,9 +2,8 @@
 
 
 ENV['RAILS_ENV'] = 'development'
-worker_processes 1
-listen 3000, :tries => 200
-
+worker_processes 2
+listen 3000, :tries => 5
 timeout 30
 
 root = File.expand_path(File.dirname(__FILE__)).split('/')[0..-2].join('/')
