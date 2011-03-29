c5c681faf43e23710fa58d30c41b586e59e08f83
Wuhsien.Yu <geniusyu@gmail.com>
2011-03-29 11:40:44 +0800

    fix xml issue

--- app/helpers/server_helper.rb
+++ app/helpers/server_helper.rb
@@ -15,9 +15,9 @@ module ServerHelper
 
   def serialize_extra_attribute(builder, key, value)
     if value.kind_of?(String)
-      builder.tag!("cas:#{key}", value)
+      builder.tag! key, value
     elsif value.kind_of?(Numeric)
-      builder.tag!("cas:#{key}", value.to_s)
+      builder.tag! key, value.to_s
     else
       builder.tag! key do
         builder.cdata! value.to_yaml
